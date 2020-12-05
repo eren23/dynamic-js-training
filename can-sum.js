1138bfd0c9009b2503972c1e6c0bed327d6fac27
eren23 <erenak1996@gmail.com>
2020-12-05 16:53:54 +0100

    can-sum

---
+++ can-sum.js
@@ -0,0 +1,32 @@
+// const canSum = (targetSum, numbers) => {
+//   if (targetSum === 0) return true;
+//   if (targetSum < 0) return false;
+
+//   for (let num of numbers) {
+//     const remainder = targetSum - num;
+//     if (canSum(remainder, numbers) === true) return true;
+//   }
+//   return false;
+// };
+
+const canSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
+
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    if (canSum(remainder, numbers, memo) === true) {
+      memo[targetSum] = true;
+      return true;
+    }
+  }
+  memo[targetSum] = false;
+  return false;
+};
+
+console.log(canSum(7, [2, 5, 7, 3, 4]));
+console.log(canSum(7, [2, 5]));
+console.log(canSum(7, [2, 4]));
+
+console.log(canSum(300, [7, 14]));
