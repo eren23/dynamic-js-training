fcaf1d0855e4c4cefa23e8fba80ad250dd813822
eren23 <erenak1996@gmail.com>
2020-12-05 13:42:19 +0100

    grid-traveler

---
+++ grid-traveler.js
@@ -0,0 +1,22 @@
+const gridTraveler = (m, n, memo = {}) => {
+  const key = m + "," + n;
+  // args in the memo
+  if (key in memo) return memo[key];
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+  memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+  return memo[key];
+};
+
+console.log(gridTraveler(3, 3));
+console.log(gridTraveler(3, 2));
+console.log(gridTraveler(2, 3));
+console.log(gridTraveler(4, 4));
+console.log(gridTraveler(18, 18));
+console.log(gridTraveler(50, 50));
+
+// const gridTraveler = (m, n) => {
+//     if (m === 1 && n === 1) return 1;
+//     if (m === 0 || n === 0) return 0;
+//     return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+//   };
