c335953283c840ee19e6c588d9424fbaba5cf345
eren23 <erenak1996@gmail.com>
2020-12-10 00:02:35 +0100

    can construct

---
+++ can-construct.js
@@ -0,0 +1,39 @@
+const canConstruct = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target];
+  if (wordBank === "") {
+    return true;
+  }
+
+  for (let word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+      if (canConstruct(suffix, wordBank, memo) === true) {
+        memo[target] = true;
+        return true;
+      }
+    }
+  }
+  memo[target] = false;
+  return false;
+};
+
+console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(
+  canConstruct("skateboard", ["bo", "rd", "ate", "t", "sk", "ska", "boar"])
+);
+console.log(
+  canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
+);
+console.log(
+  canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "ee",
+    "eeeeeee",
+    "eeeeeeeeeeeee",
+    "eeeeeeeeee",
+    "eeeeeeee",
+    "e",
+    "eee",
+    "eeee",
+    "eeeee",
+  ])
+);
